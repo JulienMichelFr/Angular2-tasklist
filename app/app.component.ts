a0e09956b84167f897a3169c0420c9f52f414fd3
Julien Michel <julienmichel.pro@gmail.com>
2016-03-26 13:19:21 +0100

    list is displayed

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,35 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    templateUrl: 'app/app.component.html'
 })
-export class AppComponent { }
+export class AppComponent {
+    tasks = [
+        {
+            id: 1,
+            title: 'Task1',
+            done: false
+        },
+        {
+            id: 2,
+            title: 'Task2',
+            done: true
+        },
+        {
+            id: 3,
+            title: 'Task3',
+            done: false
+        },
+        {
+            id: 4,
+            title: 'Task4',
+            done: false
+        },
+        {
+            id: 5,
+            title: 'Task5',
+            done: false
+        },
+
+    ]
+}
